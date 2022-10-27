c6d685c81831ebbc9f9da966e6a4d1565c9b93df
Sid Murching <sid.murching@databricks.com>
2022-10-27 11:04:32 -0700

    More fixes

--- {{cookiecutter.project_name}}/.mlops-setup-scripts/cicd/common/main.tf
+++ {{cookiecutter.project_name}}/.mlops-setup-scripts/cicd/common/main.tf
@@ -4,7 +4,7 @@ resource "databricks_git_credential" "service_principal_git_token" {
   personal_access_token = var.git_token
 }
 
-{%- if cookiecutter.cicd_platform == "gitHub" -%}
+{% if cookiecutter.cicd_platform == "gitHub" -%}
 // Store Git token for triggering CD workflows in a Databricks secret scope for
 // use by model training, in the staging and prod workspaces. We create the
 // secret scopes using our CI/CD service principals, so that only the CI/CD service
