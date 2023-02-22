00fb27379f2c13493d92a8af52c322216e0097ca
Zhe Wang <zhe.wang@databricks.com>
2023-02-22 09:59:05 -0800

    wip

--- {{cookiecutter.project_name}}/steps/predict.py
+++ {{cookiecutter.project_name}}/steps/predict.py
@@ -1,7 +1,32 @@
 import mlflow
 from pyspark.sql.functions import struct, lit, to_timestamp
 
+{% if cookiecutter.include_feature_store == "yes" %}
+def predict_batch(
+    fs_client, model_uri, input_table_name, output_table_name, model_version, ts
+):
+    """
+    Apply the model at the specified URI for batch inference on the table with name input_table_name,
+    writing results to the table with name output_table_name
+    """
+    batch_df = fs_client.read_table(input_table_name)
 
+    predict = fs_client.score_batch(
+        model_uri,
+        batch_df
+    )
+
+    output_df = (
+        table.withColumn("prediction", predict(struct(*table.columns)))
+        .withColumn("model_version", lit(model_version))
+        .withColumn("inference_timestamp", to_timestamp(lit(ts)))
+    )
+    output_df.display()
+    # Model predictions are written to the Delta table provided as input.
+    # Delta is the default format in Databricks Runtime 8.0 and above.
+    output_df.write.format("delta").mode("overwrite").saveAsTable(output_table_name)
+
+{% else %}
 def predict_batch(
     spark_session, model_uri, input_table_name, output_table_name, model_version, ts
 ):
@@ -22,3 +47,5 @@ def predict_batch(
     # Model predictions are written to the Delta table provided as input.
     # Delta is the default format in Databricks Runtime 8.0 and above.
     output_df.write.format("delta").mode("overwrite").saveAsTable(output_table_name)
+
+{% endif %}
