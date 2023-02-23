44f22500de72a83e6209f17f14379777e3b567c8
Zhe Wang <zhe.wang@databricks.com>
2023-02-23 09:54:37 -0800

    [ML-27370] Add batch score inference using feature store (#58)
    * wip
    
    * wip
    
    * Update BatchInference.py
    
    * wip
    
    * wip

--- {{cookiecutter.root_dir__update_if_you_intend_to_use_monorepo}}/{{cookiecutter.project_name}}/deployment/batch_inference/predict.py
+++ {{cookiecutter.root_dir__update_if_you_intend_to_use_monorepo}}/{{cookiecutter.project_name}}/deployment/batch_inference/predict.py
@@ -10,6 +10,21 @@ def predict_batch(
     writing results to the table with name output_table_name
     """
     table = spark_session.table(input_table_name)
+    {% if cookiecutter.include_feature_store == "yes" %}
+    from databricks.feature_store import FeatureStoreClient
+    
+    fs_client = FeatureStoreClient()
+
+    prediction_df = fs_client.score_batch(
+        model_uri,
+        table
+    )
+    output_df = (
+        table.withColumn("prediction", prediction_df["prediction"])
+        .withColumn("model_version", lit(model_version))
+        .withColumn("inference_timestamp", to_timestamp(lit(ts)))
+    )
+    {% else %}
     predict = mlflow.pyfunc.spark_udf(
         spark_session, model_uri, result_type="string", env_manager="conda"
     )
@@ -18,6 +33,7 @@ def predict_batch(
         .withColumn("model_version", lit(model_version))
         .withColumn("inference_timestamp", to_timestamp(lit(ts)))
     )
+    {% endif %}
     output_df.display()
     # Model predictions are written to the Delta table provided as input.
     # Delta is the default format in Databricks Runtime 8.0 and above.
