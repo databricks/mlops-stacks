2dde9e1d2fba1eca7c6b767bd41adeb11e21d00b
Mingyu Li <mingyu.li@databricks.com>
2023-03-20 16:31:29 -0700

    Update terraform file format
    Signed-off-by: Mingyu Li <mingyu.li@databricks.com>

--- {{cookiecutter.root_dir__update_if_you_intend_to_use_monorepo}}/{{cookiecutter.project_name}}/terraform/prod/training-job.tf
+++ {{cookiecutter.root_dir__update_if_you_intend_to_use_monorepo}}/{{cookiecutter.project_name}}/terraform/prod/training-job.tf
@@ -16,7 +16,7 @@ resource "databricks_job" "model_training_job" {
     {% if cookiecutter.include_feature_store == "yes" %}notebook_task {
       notebook_path = "{{cookiecutter.project_name}}/training/notebooks/TrainWithFeatureStore"
       base_parameters = {
-        env                = local.env
+        env = local.env
         # TODO: Update training_data_path
         training_data_path = "/databricks-datasets/nyctaxi-with-zipcodes/subsampled"
         experiment_name    = databricks_mlflow_experiment.experiment.name
