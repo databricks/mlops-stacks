06c3f1f3be2b3a7ed3f4644b4c3bb6cba094a217
Eric Golinko <eric.golinko@databricks.com>
2023-07-13 14:51:37 -0400

    updated logic for batch

--- {{cookiecutter.root_dir__update_if_you_intend_to_use_monorepo}}/{{cookiecutter.project_name_alphanumeric_underscore}}/deployment/batch_inference/predict.py
+++ {{cookiecutter.root_dir__update_if_you_intend_to_use_monorepo}}/{{cookiecutter.project_name_alphanumeric_underscore}}/deployment/batch_inference/predict.py
@@ -9,8 +9,40 @@ def predict_batch(
     Apply the model at the specified URI for batch inference on the table with name input_table_name,
     writing results to the table with name output_table_name
     """
-    table = spark_session.table(input_table_name)
+
     {% if cookiecutter.include_feature_store == "yes" %}
+    def rounded_unix_timestamp(dt, num_minutes=15):
+        """
+            Ceilings datetime dt to interval num_minutes, then returns the unix timestamp.
+        """
+        
+        nsecs = dt.minute * 60 + dt.second + dt.microsecond * 1e-6
+        delta = math.ceil(nsecs / (60 * num_minutes)) * (60 * num_minutes) - nsecs
+        return int((dt + timedelta(seconds=delta)).replace(tzinfo=timezone.utc).timestamp())
+
+    rounded_unix_timestamp_udf = udf(rounded_unix_timestamp, IntegerType())
+
+    _table = spark_session.table(input_table_name)
+    
+    table = (
+        _table.withColumn(
+            "rounded_pickup_datetime",
+            to_timestamp(
+                rounded_unix_timestamp_udf(
+                    _table["tpep_pickup_datetime"], lit(15)
+                    )
+                )
+            )
+        .withColumn(
+            "rounded_dropoff_datetime",
+            to_timestamp(
+                rounded_unix_timestamp_udf(
+                    _table["tpep_dropoff_datetime"], lit(30)
+                    )
+                )
+            )
+        )
+
     from databricks.feature_store import FeatureStoreClient
     
     fs_client = FeatureStoreClient()
@@ -19,12 +51,15 @@ def predict_batch(
         model_uri,
         table
     )
+
     output_df = (
         prediction_df.withColumn("prediction", prediction_df["prediction"])
         .withColumn("model_version", lit(model_version))
         .withColumn("inference_timestamp", to_timestamp(lit(ts)))
     )
     {% else %}
+    table = spark_session.table(input_table_name)
+
     predict = mlflow.pyfunc.spark_udf(
         spark_session, model_uri, result_type="string", env_manager="conda"
     )
