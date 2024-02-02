ecf542bc8e6c46dc1eb409c3bab25ff275d08937
Arpit Jasapara <arpit.jasapara@databricks.com>
2024-02-01 20:15:09 -0800

    Update tests

--- tests/test_github_actions.py
+++ tests/test_github_actions.py
@@ -1,20 +1,35 @@
 import subprocess
 import pytest
+from functools import wraps
 from utils import (
     databricks_cli,
     generated_project_dir,
     parametrize_by_cloud,
-    parametrize_by_project_generation_params,
 )
+
+
+def parametrize_by_params(fn):
+    @wraps(fn)
+    @pytest.mark.parametrize(
+        "setup_cicd_and_project,include_feature_store,include_mlflow_recipes,include_models_in_unity_catalog",
+        [
+            ("CICD_and_Project", "no", "no", "no"),
+            ("CICD_and_Project", "no", "no", "yes"),
+            ("CICD_and_Project", "no", "yes", "no"),
+            ("CICD_and_Project", "yes", "no", "no"),
+            ("CICD_and_Project", "yes", "no", "yes"),
+        ],
+    )
+    def wrapper(*args, **kwargs):
+        return fn(*args, **kwargs)
+
+    return wrapper
 
 
 @pytest.mark.parametrize(
     "cicd_platform", ["github_actions", "github_actions_for_github_enterprise_servers"]
 )
-@pytest.mark.parametrize(
-    "include_feature_store, include_mlflow_recipes, include_models_in_unity_catalog",
-    [("yes", "no", "no"), ("no", "yes", "no"), ("no", "no", "yes"), ("no", "no", "no")],
-)
+@parametrize_by_params
 @parametrize_by_cloud
 def test_generated_yaml_format(cicd_platform, generated_project_dir):
     # Note: actionlint only works when the directory is a git project. Thus we begin by initiatilizing
@@ -36,38 +51,10 @@ def test_generated_yaml_format(cicd_platform, generated_project_dir):
 @pytest.mark.parametrize(
     "cicd_platform", ["github_actions", "github_actions_for_github_enterprise_servers"]
 )
-@pytest.mark.parametrize("include_feature_store", ["no"])
-@pytest.mark.parametrize(
-    "include_mlflow_recipes, include_models_in_unity_catalog",
-    [("yes", "no"), ("no", "yes"), ("no", "no")],
-)
+@parametrize_by_params
 @parametrize_by_cloud
 def test_run_unit_tests_workflow(cicd_platform, generated_project_dir):
     """Test that the GitHub workflow for running unit tests in the materialized project passes"""
-    # We only test the unit test workflow, as it's the only one that doesn't require
-    # Databricks REST API
-    subprocess.run(
-        """
-        git init
-        act -s GITHUB_TOKEN workflow_dispatch --workflows .github/workflows/my-mlops-project-run-tests.yml -j "unit_tests"
-        """,
-        shell=True,
-        check=True,
-        executable="/bin/bash",
-        cwd=(generated_project_dir / "my-mlops-project"),
-    )
-
-
-@pytest.mark.large
-@pytest.mark.parametrize(
-    "cicd_platform", ["github_actions", "github_actions_for_github_enterprise_servers"]
-)
-@pytest.mark.parametrize("include_feature_store", ["yes"])
-@pytest.mark.parametrize("include_mlflow_recipes", ["no"])
-@pytest.mark.parametrize("include_models_in_unity_catalog", ["no"])
-@parametrize_by_cloud
-def test_run_unit_tests_feature_store_workflow(cicd_platform, generated_project_dir):
-    """Test that the GitHub workflow for running unit tests passes for feature store"""
     # We only test the unit test workflow, as it's the only one that doesn't require
     # Databricks REST API
     subprocess.run(
