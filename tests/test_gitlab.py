95ec8a98719570f0ecfeb40ed1e28d2434d5b1cf
Valerio P <valerio.pa@gmail.com>
2024-11-20 19:26:23 +0000

    review test gitlab

--- tests/test_gitlab.py
+++ tests/test_gitlab.py
@@ -11,25 +11,24 @@ from utils import (
 @pytest.mark.parametrize("cicd_platform", ["gitlab"])
 @pytest.mark.parametrize(
     "setup_cicd_and_project,include_feature_store,include_mlflow_recipes,include_models_in_unity_catalog",
-    [        
+    [
         ("CICD_and_Project", "no", "no", "no"),
         ("CICD_and_Project", "no", "no", "yes"),
         ("CICD_and_Project", "no", "yes", "no"),
         ("CICD_and_Project", "yes", "no", "no"),
         ("CICD_and_Project", "yes", "no", "yes"),
-        ("CICD_Only", "no", "no", "no")
+        ("CICD_Only", "no", "no", "no"),
     ],
 )
 @parametrize_by_cloud
-@pytest.mark.parametrize("cloud", ["azure"])
-def test_generated_yaml_format(
+def test_generated_gitlab_folder(
     cloud, include_models_in_unity_catalog, generated_project_dir
 ):
-    print("generated_project_dir:", generated_project_dir)
+    if cloud == "gcp" and include_models_in_unity_catalog == "yes":
+        # Skip test for GCP with Unity Catalog
+        return
 
     # TEST: Check if gitlab folder has been created.
-    # TODO Check syntax with: gitlab-ci-local --file ./.gitlab/cicd.yml
-    # (NOTE  requires gitlab-ci-local installed on ubuntu)
     subprocess.run(
         """
         ls ./.gitlab/pipelines
@@ -39,3 +38,5 @@ def test_generated_yaml_format(
         executable="/bin/bash",
         cwd=(generated_project_dir / "my-mlops-project"),
     )
+    # TODO Check syntax with: gitlab-ci-local --file ./.gitlab/cicd.yml
+    # (NOTE: syntax check requires gitlab-ci-local installed on VM)
